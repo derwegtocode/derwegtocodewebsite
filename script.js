616e3c2995188164df8113b62eab039e9f2b6e67
Maria Lujan Sosa Balsa <luubalsa@gmail.com>
2022-11-04 14:33:15 +0100

    termiar contact

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+const cvButton = document.getElementsByClassName('cv')[0];
+const cvDiv = document.getElementsByClassName('cv-div')[0];
+
+cvButton.addEventListener('click', () => {
+  if (cvDiv.style.display === 'none' ) {
+    cvDiv.style.display = 'flex';
+  } else {
+    cvDiv.style.display = 'none';
+  } 
+})
